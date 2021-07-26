d8fd6a4d23da55068f280633e193c5f71e0b4bc4
G184302021 <86222176+G184302021@users.noreply.github.com>
2021-07-26 16:57:49 +0900

    ゲームが完成した。

--- main.ts
+++ main.ts
@@ -1,1 +1,57 @@
 scene.setBackgroundColor(8)
+let spacePlane = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . 6 6 6 6 9 9 9 9 9 9 9 . . . .
+    . . . 6 9 9 9 9 9 9 9 1 1 . . .
+    . . 9 9 6 6 6 6 6 9 9 9 9 9 . .
+    . . . 6 6 6 6 6 6 6 9 9 9 9 9 .
+    . . 6 6 6 6 6 6 . . . . . . . .
+    . 6 6 6 6 6 . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Player)
+info.setLife(3)
+spacePlane.setStayInScreen(true)
+controller.moveSprite(spacePlane, 200, 200)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    let missile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . 6 . . . . . . . . .
+            . . . . . . 6 6 . . . . . . . .
+            . . . 4 4 6 6 6 6 . . . . . . .
+            . . 4 4 2 9 9 9 9 9 9 9 4 4 . .
+            . . . 4 4 6 6 6 6 . . . . . . .
+            . . . . . . 6 6 . . . . . . . .
+            . . . . . . 6 . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+        `, spacePlane, 200, 0)
+})
+game.onUpdateInterval(500, function on_update() {
+    let bogy = sprites.create(assets.image`teki`, SpriteKind.Enemy)
+    bogy.setVelocity(-100, randint(-30, 30))
+    bogy.y = randint(0, scene.screenHeight())
+    bogy.left = scene.screenWidth()
+    bogy.setFlag(SpriteFlag.AutoDestroy, true)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_hit(sprite: Sprite, othersprite: Sprite) {
+    othersprite.destroy(effects.fire, 100)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_crash(sprite: Sprite, othersprite: Sprite) {
+    othersprite.destroy()
+    info.changeLifeBy(-1)
+})
